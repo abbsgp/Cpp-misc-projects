dd936a3a25905e388d615c1e0a7070fd3f760408
abbsgp <abegailpalad7812@gmail.com>
2024-05-23 13:37:42 -0700

    initial push with minimal code. First up, tic tac toe!

---
+++ tictactoe/ttt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    char player1 = 'x';
+    char player2 = 'o';
+    char currentPlayer = player1;
+
+    int board[3][3];
+    int index; // position on the board
+    cout << "   " << "|" << "   " << "|" << "   " << endl;
+    cout << "---" << "|" << "---" << "|" << "---" << endl;
+    cout << "   " << "|" << "   " << "|" << "   " << endl;
+    cout << "---" << "|" << "---" << "|" << "---" << endl;
+    cout << "   " << "|" << "   " << "|" << "   " << endl;
+
+
+
+    return 0;
+}
