49954dcd255e7099511c41503547622e81d9eee6
abbsgp <abegailpalad7812@gmail.com>
2024-05-31 11:01:10 -0700

    Added a new function to mark player's positions in the board

--- tictactoe/ttt.cpp
+++ tictactoe/ttt.cpp
@@ -2,9 +2,10 @@
 
 using namespace std;
 
-bool checkForWinner(int board[3][3]);
-bool checkTie(int board[3][3]);
-void displayBoard(int board[3][3]);
+bool checkForWinner(char board[3][3]);
+bool checkTie(char board[3][3]);
+bool markSpot(char board[3][3], int pos, char currentPlayer);
+void displayBoard(char board[3][3]);
 
 int main(){
 
@@ -12,32 +13,69 @@ int main(){
     char player1 = 'X';
     char player2 = 'O';
     char currentPlayer = player1;
+    bool gameOver = false;
 
-    int board[3][3];
-    int index[9]; // position on the board
-    int pos = 0; 
-    int key; // user input 
-    bool result; // check if winner or tie is reached
+    char board[3][3] = {'1', '2', '3', 
+                        '4', '5', '6',
+                        '7', '8', '9'
+                        };
+    int pos; 
 
-    // assign elements from the 2D array to an index position (assigned with an array value that is)
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            index[pos] = board[i][j];
-            pos++;
-            // cout << pos << "  " << i << "  " << j << endl;
+    displayBoard(board);
+
+    // while it's not true (game is not over), continue
+    while(!gameOver){
+        cout << currentPlayer << ", please choose a position on the board" << endl;
+        cin >> pos;
+
+        markSpot(board, pos, currentPlayer);
+
+        if(checkForWinner(board) == true){
+            gameOver = true;
+        } else if (checkTie(board)) {
+            gameOver = true;
         }
-    }
 
-    cout << currentPlayer << ", please choose a value on the board to assign 'X'" << endl;
+        displayBoard(board);
+
+        currentPlayer = (currentPlayer == player1)? player2 : player1;
+
+    }
 
     return 0;
 }
 
-void displayBoard(int board[3][3]){
-    
+void displayBoard(char board[3][3]) 
+{ 
+    cout << "-------------\n"; 
+    for (int i = 0; i < 3; i++) { 
+        cout << "| "; 
+        for (int j = 0; j < 3; j++) { 
+            cout << (char)board[i][j] << " | "; 
+        } 
+        cout << "\n-------------\n"; 
+    } 
+} 
+
+bool markSpot(char board[3][3], int pos, char currentPlayer){
+    /*  let's say pos = 7, that is the 3rd row and first column, but in programming terms, it's row 2 column 0, so:
+    row -> (7 - 1) / 3 = 6 / 3 = 2
+    col -> (7 - 1) % 3 = 1 = 6 % 3 = 0
+    board[row][column] = board[2][0]
+    */
+    int row = (pos-1) / 3;
+    int col = (pos-1) % 3;
+
+    if(board[row][col] == 'X' || board[row][col] == 'O'){
+        return false;
+    }
+
+    board[row][col] = currentPlayer;
+
+    return true;
 }
 
-bool checkForWinner(int board[3][3]){
+bool checkForWinner(char board[3][3]){
 
     for(int i = 0; i < 3; i++){
 
@@ -53,7 +91,7 @@ bool checkForWinner(int board[3][3]){
     return false;
 }
 
-bool checkTie(int board[3][3]){
+bool checkTie(char board[3][3]){
 
     //checks if any positions on the board are empty, if so, return false
     for(int i = 0; i < 3; i++){
